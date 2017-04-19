11b9700f27af24d3d5b66607803ed7fcf3b43faf
benatespina <benatespina@gmail.com>
2017-04-19 12:22:18 +0200

    Added catchable exceptions option inside new translatable action type

--- src/LIN3S/CMSKernel/Infrastructure/Lin3sAdminBundle/Action/Type/NewTranslatableActionType.php
+++ src/LIN3S/CMSKernel/Infrastructure/Lin3sAdminBundle/Action/Type/NewTranslatableActionType.php
@@ -14,11 +14,13 @@ namespace LIN3S\CMSKernel\Infrastructure\Lin3sAdminBundle\Action\Type;
 use LIN3S\AdminBundle\Configuration\Model\Entity;
 use LIN3S\AdminBundle\Configuration\Type\ActionType;
 use LIN3S\SharedKernel\Application\CommandBus;
+use LIN3S\SharedKernel\Exception\Exception;
 use Symfony\Component\Form\FormFactoryInterface;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Flash\FlashBagInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
@@ -49,33 +51,35 @@ class NewTranslatableActionType implements ActionType
     public function execute($entity, Entity $config, Request $request, $options = null)
     {
         $entityName = $config->name();
-        $this->checkFormIsPassed($options);
         $locale = $request->query->get('locale');
 
-//        TODO: Check if locale is defined in the bundle configuration
-//        if (whatever) {
-//            throw new NotFoundHttpException(
-//                sprintf('%s locale is not supported from the admin', $locale)
-//            );
-//        }
+        $this->checkFormIsPassed($options);
+        $this->checkLocaleIsAvailable($locale);
 
         $form = $this->formFactory->create($options['form'], null, ['locale' => $locale]);
         if ($request->isMethod('POST') || $request->isMethod('PUT') || $request->isMethod('PATCH')) {
             $form->handleRequest($request);
             if ($form->isValid() && $form->isSubmitted()) {
-                $command = $form->getData();
+                try {
+                    $command = $form->getData();
+                    $this->commandBus->handle($command);
 
-                $this->commandBus->handle($command);
-                $this->flashBag->add(
-                    'lin3s_admin_success',
-                    sprintf('The %s translation is successfully saved', $entityName)
-                );
+                    $this->flashBag->add(
+                        'lin3s_admin_success',
+                        sprintf('The %s translation is successfully saved', $entityName)
+                    );
 
-                return $this->redirect($options, $entityName, $command->id());
+                    return $this->redirect($options, $entityName, $command->id());
+                } catch (Exception $exception) {
+                    $this->addError($exception, $options);
+                }
             } else {
                 $this->flashBag->add(
                     'lin3s_admin_error',
-                    sprintf('Errors while saving %s translation. Please check all fields and try again', $entityName)
+                    sprintf(
+                        'Errors while saving %s translation. Please check all fields and try again',
+                        $entityName
+                    )
                 );
             }
         }
@@ -99,6 +103,15 @@ class NewTranslatableActionType implements ActionType
         }
     }
 
+    private function checkLocaleIsAvailable($locale)
+    {
+        if (false) { // TODO: Check if locale is defined in the bundle configuration
+            throw new NotFoundHttpException(
+                sprintf('%s locale is not supported from the admin', $locale)
+            );
+        }
+    }
+
     private function redirect($options, $entity, $id)
     {
         if (!isset($options['redirectAction'])) {
@@ -116,5 +129,24 @@ class NewTranslatableActionType implements ActionType
                 'id'     => $id,
             ])
         );
+    }
+
+    private function addError(Exception $exception, array $options)
+    {
+        $exceptions = $this->catchableExceptions($options);
+        $exceptionClassName = get_class($exception);
+
+        if (array_key_exists($exceptionClassName, $exceptions)) {
+            $this->flashBag->add('lin3s_admin_error', $exceptions[$exceptionClassName]);
+        }
+    }
+
+    private function catchableExceptions(array $options)
+    {
+        if (!isset($options['catchable_exceptions'])) {
+            return [];
+        }
+
+        return json_decode($options['catchable_exceptions'], true);
     }
 }
